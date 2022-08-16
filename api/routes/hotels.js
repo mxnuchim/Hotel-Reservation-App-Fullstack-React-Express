b3584f850abae8167a44aa9a319512ee72696c0e
mxnuchim <manuchimoliver779@gmail.com>
2022-08-16 15:53:43 +0100

    Added api and unfinished admin panel

---
+++ api/routes/hotels.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel, countByCity, countByType } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/find/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", );
+
+export default router;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,14 +1,26 @@
 import "./Navbar.css"
+import {Link} from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+
+
 
 const Navbar = () => {
+
+  const { user } = useContext(AuthContext)
+
   return (
     <div className='navbar'>
         <div className="navContainer">
+          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
             <span className="logo">Dwell</span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-            </div>
+          </Link>
+            {user ? user.username : (
+              <div className="navItems">
+                  <button className="navButton">Register</button>
+                  <button className="navButton">Login</button>
+              </div>
+            )}
         </div>
     </div>
   )
